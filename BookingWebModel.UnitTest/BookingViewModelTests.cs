b08033fb4c444ac0655f28eb68d15711c3cc884b
Mark Seemann <mark@seemann.ms>
2011-10-02 21:06:48 +0200

    Defined BookingViewModel as a standard View Model.

---
+++ BookingWebModel.UnitTest/BookingViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Samples.Booking.WebUI.Models;
+
+namespace Ploeh.Samples.Booking.WebModel.UnitTest
+{
+    public class BookingViewModelTests
+    {
+        [Fact]
+        public void SutHasDefaultConstructor()
+        {
+            Assert.DoesNotThrow(() => 
+                new BookingViewModel());
+        }
+    }
+}
