6d19ded93790bc982270e29f0eff0b678d1a87fa
Mark Seemann <mark@seemann.ms>
2011-10-03 09:15:26 +0200

    Added DisabledDateController.

---
+++ BookingWebModel.UnitTest/DisabledDatesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+using System.Web.Mvc;
+using Ploeh.Samples.Booking.WebUI.Controllers;
+
+namespace Ploeh.Samples.Booking.WebModel.UnitTest
+{
+    public class DisabledDatesControllerTests
+    {
+        [Theory, AutoWebData]
+        public void SutIsController(DisabledDatesController sut)
+        {
+            Assert.IsAssignableFrom<IController>(sut);
+        }
+    }
+}
