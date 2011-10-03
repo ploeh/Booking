f7d376e7103247cff13c1a138fa20fa3e77ac450
Mark Seemann <mark@seemann.ms>
2011-10-03 08:48:45 +0200

    Added [HttpPost] attribute to Make operation.

--- BookingWebUI/Controllers/BookingController.cs
+++ BookingWebUI/Controllers/BookingController.cs
@@ -14,6 +14,7 @@ namespace Ploeh.Samples.Booking.WebUI.Controllers
             return this.View(new BookingViewModel { Date = new DateTime(year, month, day) });
         }
 
+        [HttpPost]
         public ViewResult Make(BookingViewModel model)
         {
             return this.View("Receipt");
