bc7435a1c3f1a2d71e60a72917fcc7d7d93e214f
Mark Seemann <mark@seemann.ms>
2011-10-03 15:55:34 +0200

    Added DateViewModelBinder.

---
+++ BookingWebModel.UnitTest/DateViewModelBinderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+using Ploeh.Samples.Booking.WebModel;
+using System.Web.Mvc;
+
+namespace Ploeh.Samples.Booking.WebModel.UnitTest
+{
+    public class DateViewModelBinderTests
+    {
+        [Theory, AutoWebData]
+        public void SutIsModelBinder(DateViewModelBinder sut)
+        {
+            Assert.IsAssignableFrom<IModelBinder>(sut);
+        }
+    }
+}
