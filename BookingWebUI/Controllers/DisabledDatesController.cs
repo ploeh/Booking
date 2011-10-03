1abf073cc8e469d6093dfd9b6379ba2c87325aff
Mark Seemann <mark@seemann.ms>
2011-10-03 09:19:48 +0200

    Added [OutputCache] attribute to Get operation.

--- BookingWebUI/Controllers/DisabledDatesController.cs
+++ BookingWebUI/Controllers/DisabledDatesController.cs
@@ -8,6 +8,7 @@ namespace Ploeh.Samples.Booking.WebUI.Controllers
 {
     public class DisabledDatesController : Controller
     {
+        [OutputCache(Duration = 0, VaryByParam = "none")]
         public JsonResult Get(int year, int month)
         {
             return this.Json(new string[0]);
