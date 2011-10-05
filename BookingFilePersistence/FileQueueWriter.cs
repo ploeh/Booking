699bd9dbf63a7894342af8ce491ec96ce107d8fe
Mark Seemann <mark@seemann.ms>
2011-10-05 20:43:00 +0200

    Added FileQueueWriter class.

---
+++ BookingFilePersistence/FileQueueWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Booking.PersistenceModel;
+using System.IO;
+using Ploeh.Samples.Booking.DomainModel;
+
+namespace Ploeh.Samples.Booking.Persistence.FileSystem
+{
+    public class FileQueueWriter<T> : IStoreWriter<T> where T : IMessage
+    {
+        private readonly DirectoryInfo directory;
+
+        public FileQueueWriter(DirectoryInfo directory)
+        {
+            this.directory = directory;
+        }
+
+        public Stream GetStreamFor(T item)
+        {
+            var path = Path.ChangeExtension(
+                Path.Combine(
+                    this.directory.FullName,
+                    item.Id.ToString()),
+                "txt");
+            return File.Open(path, FileMode.Create);
+        }
+    }
+}
