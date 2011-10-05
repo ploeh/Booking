cd8bd8dfd138c9d81c7531ba7eab5b200ed3fc1c
Mark Seemann <mark@seemann.ms>
2011-10-05 15:36:36 +0200

    Added JsonChannel class.

---
+++ BookingJson/JsonChannel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Booking.DomainModel;
+
+namespace Ploeh.Samples.Booking.JsonAntiCorruption
+{
+    public class JsonChannel<T> : IChannel<T> where T : IMessage
+    {
+        public void Send(T message)
+        {
+        }
+    }
+}
