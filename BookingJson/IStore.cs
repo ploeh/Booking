0d769e67675966c4d8b8069946fe6f63e55484b8
Mark Seemann <mark@seemann.ms>
2011-10-05 17:43:08 +0200

    Implemented Send method.

---
+++ BookingJson/IStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Ploeh.Samples.Booking.JsonAntiCorruption
+{
+    public interface IStore<T>
+    {
+        Stream GetStreamFor(T item);
+    }
+}
--- BookingJson/JsonChannel.cs
+++ BookingJson/JsonChannel.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Ploeh.Samples.Booking.DomainModel;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Ploeh.Samples.Booking.JsonAntiCorruption
 {
     public class JsonChannel<T> : IChannel<T> where T : IMessage
     {
+        private readonly IStore<T> store;
+        private readonly JsonSerializer serializer;
+
+        public JsonChannel(IStore<T> store)
+        {
+            this.store = store;
+            this.serializer = new JsonSerializer();
+        }
+
         public void Send(T message)
         {
+            using (var stream = this.store.GetStreamFor(message))
+            using (var writer = new StreamWriter(stream))
+                this.serializer.Serialize(writer, message);
         }
     }
 }
