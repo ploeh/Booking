9a8d9731b6a271efb2fac2a520658a9550c13e9d
Mark Seemann <mark@seemann.ms>
2011-10-05 21:05:05 +0200

    Composed BookingController with proper channel.

--- BookingWebUI/PoorMansCompositionRoot.cs
+++ BookingWebUI/PoorMansCompositionRoot.cs
@@ -6,18 +6,34 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using Ploeh.Samples.Booking.WebModel;
 using Ploeh.Samples.Booking.DomainModel;
+using Ploeh.Samples.Booking.JsonAntiCorruption;
+using Ploeh.Samples.Booking.Persistence.FileSystem;
+using System.IO;
+using System.Web.Hosting;
 
 namespace Ploeh.Samples.Booking.WebUI
 {
     public class PoorMansCompositionRoot : DefaultControllerFactory
     {
+        private readonly DirectoryInfo queueDirectory;
+
+        public PoorMansCompositionRoot()
+        {
+            var queuePath = HostingEnvironment.MapPath("~/queue");
+            if (!Directory.Exists(queuePath))
+                Directory.CreateDirectory(queuePath);
+            this.queueDirectory = new DirectoryInfo(queuePath);
+        }
+
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
             if (controllerType == typeof(BookingController))
             {
                 return new BookingController(
                     new FixedRemainingCapacityReader(),
-                    new NullChannel<MakeReservationCommand>());
+                    new JsonChannel<MakeReservationCommand>(
+                        new FileQueueWriter<MakeReservationCommand>(
+                            this.queueDirectory)));
             }
 
             return base.GetControllerInstance(requestContext, controllerType);
