b41b18724719aa61ada0b76be8cb4ca1f6e9fa28
Mark Seemann <mark@seemann.ms>
2011-10-05 12:42:27 +0200

    Added Body property to Envelope class.

---
+++ BookingDomainModel.UnitTest/EnvelopeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+using Ploeh.AutoFixture.Xunit;
+using Ploeh.Samples.Booking.DomainModel;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public abstract class EnvelopeTests<T>
+    {
+        [Theory, AutoDomainData]
+        public void BodyIsCorrectWhenConstructedModestly([Frozen]T expected,
+            [Modest]Envelope<T> sut)
+        {
+            T actual = sut.Body;
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class EnvelopeTestsOfObject : EnvelopeTests<object> { }
+    public class EnvelopeTestsOfString : EnvelopeTests<string> { }
+    public class EnvelopeTestsOfInt32 : EnvelopeTests<int> { }
+    public class EnvelopeTestsOfGuid : EnvelopeTests<Guid> { }
+    public class EnvelopeTestsOfDateTime : EnvelopeTests<DateTime> { }
+    public class EnvelopeTestsOfVersion : EnvelopeTests<Version> { }
+}
---
+++ BookingDomainModel/Envelope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Samples.Booking.DomainModel
+{
+    public class Envelope<T>
+    {
+        private readonly T body;
+
+        public Envelope(T body)
+        {
+            this.body = body;
+        }
+
+        public T Body
+        {
+            get { return this.body; }
+        }
+    }
+}
