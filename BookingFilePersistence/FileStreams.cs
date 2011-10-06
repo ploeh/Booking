b4f5fb3154af0cc4a5622ce4787aa5272cb09b98
Mark Seemann <mark@seemann.ms>
2011-10-06 14:32:30 +0200

    Added FileStreams.

---
+++ BookingFilePersistence/FileStreams.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace Ploeh.Samples.Booking.Persistence.FileSystem
+{
+    public class FileStreams : IEnumerable<Stream>
+    {
+        private readonly DirectoryInfo directory;
+        private readonly string extension;
+
+        public FileStreams(DirectoryInfo directory, string extension)
+        {
+            this.directory = directory;
+            this.extension = extension;
+        }
+
+        public IEnumerator<Stream> GetEnumerator()
+        {
+            while(true)
+            {
+                var file = this.directory.EnumerateFiles("*." + this.extension).FirstOrDefault();
+                if (file == null)
+                    yield break;
+
+                yield return file.OpenRead();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
