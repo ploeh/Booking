26e185d5725e676535e63895a86933388cce69b0
Mark Seemann <mark@seemann.ms>
2011-10-06 14:50:57 +0200

    Wired up basics of Main in Daemon.

--- BookingDaemon/Program.cs
+++ BookingDaemon/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Ploeh.Samples.Booking.Persistence.FileSystem;
+using System.IO;
+using Ploeh.Samples.Booking.PersistenceModel;
+using System.Threading;
 
 namespace Ploeh.Samples.Booking.Daemon
 {
@@ -9,6 +13,31 @@ namespace Ploeh.Samples.Booking.Daemon
     {
         static void Main(string[] args)
         {
+            var q = new PollingConsumer(
+                new FileStreams(
+                    new DirectoryInfo(@"C:\Users\mark\Documents\Presentations\Conventions\Booking\BookingWebUI\Queue"),
+                    "txt"),
+                new NullObserver<Stream>());
+            while (true)
+            {
+                q.ConsumeSequence();
+                Thread.Sleep(500);
+            }
+        }
+
+        private class NullObserver<T> : IObserver<T>
+        {
+            public void OnCompleted()
+            {
+            }
+
+            public void OnError(Exception error)
+            {
+            }
+
+            public void OnNext(T value)
+            {
+            }
         }
     }
 }
