fea7e87d3a4d7c62f7986a0fa68077926881091d
Mark Seemann <mark@seemann.ms>
2011-10-06 18:48:01 +0200

    Introduced ReservationRejectedEvent.

---
+++ BookingDomainModel.UnitTest/ReservertionRejectedEventTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Booking.DomainModel;
+using Xunit.Extensions;
+using Xunit;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public class ReservertionRejectedEventTests
+    {
+        [Theory, AutoDomainData]
+        public void SutIsMessage(ReservationRejectedEvent sut)
+        {
+            Assert.IsAssignableFrom<IMessage>(sut);
+        }
+    }
+}
---
+++ BookingDomainModel/ReservationRejectedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Samples.Booking.DomainModel
+{
+    public class ReservationRejectedEvent : IMessage
+    {
+        public Envelope Envelop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Id
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
