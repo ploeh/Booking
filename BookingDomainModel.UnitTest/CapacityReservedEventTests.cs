05f9d9159ac6617668d7e99c28b1702d41763ee2
Mark Seemann <mark@seemann.ms>
2011-10-06 11:26:54 +0200

    Added CapacityReservedEvent.

---
+++ BookingDomainModel.UnitTest/CapacityReservedEventTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Ploeh.Samples.Booking.DomainModel;
+using Xunit;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public class CapacityReservedEventTests
+    {
+        [Theory, AutoDomainData]
+        public void SutIsMessage(CapacityReservedEvent sut)
+        {
+            Assert.IsAssignableFrom<IMessage>(sut);
+        }
+    }
+}
