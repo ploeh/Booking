7e17f47147903964470d4dd347abbd61f55bc71c
Mark Seemann <mark@seemann.ms>
2011-10-06 21:00:04 +0200

    Introduced CapactityGate.

---
+++ BookingDomainModel.UnitTest/CapacityGateTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Ploeh.Samples.Booking.DomainModel;
+using Xunit;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public class CapacityGateTests
+    {
+        [Theory, AutoDomainData]
+        public void SutIsConsumer(CapacityGate sut)
+        {
+            Assert.IsAssignableFrom<IConsumer<RequestReservationCommand>>(sut);
+        }
+    }
+}
