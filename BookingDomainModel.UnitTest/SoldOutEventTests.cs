dab7d8808f5537b665359fd918236287c62c224a
Mark Seemann <mark@seemann.ms>
2011-10-06 22:14:38 +0200

    Introduced SoldOutEvent.

---
+++ BookingDomainModel.UnitTest/SoldOutEventTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Ploeh.Samples.Booking.DomainModel;
+using Xunit;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public class SoldOutEventTests
+    {
+        [Theory, AutoDomainData]
+        public void SutIsMessage(SoldOutEvent sut)
+        {
+            Assert.IsAssignableFrom<IMessage>(sut);
+        }
+    }
+}
