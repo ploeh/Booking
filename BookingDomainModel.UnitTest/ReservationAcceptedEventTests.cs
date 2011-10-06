ab0299ddcd6772f27797703c2abcb9d940cd7fef
Mark Seemann <mark@seemann.ms>
2011-10-06 18:20:26 +0200

    Added ReservationAcceptedEvent.

---
+++ BookingDomainModel.UnitTest/ReservationAcceptedEventTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+using Ploeh.Samples.Booking.DomainModel;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public class ReservationAcceptedEventTests
+    {
+        [Theory, AutoDomainData]
+        public void SutIsMessage(ReservationAcceptedEvent sut)
+        {
+            Assert.IsAssignableFrom<IMessage>(sut);
+        }
+    }
+}
