c3c1c9d9d034849703f1cef1a56b90cccdd09890
Mark Seemann <mark@seemann.ms>
2011-10-07 16:21:41 +0200

    Added reader to DisabledDatesController.

--- BookingWebModel.UnitTest/DisabledDatesControllerTests.cs
+++ BookingWebModel.UnitTest/DisabledDatesControllerTests.cs
@@ -5,6 +5,9 @@ using System.Text;
 using Xunit.Extensions;
 using Xunit;
 using System.Web.Mvc;
+using Ploeh.AutoFixture.Xunit;
+using Ploeh.Samples.Booking.DomainModel;
+using Moq;
 
 namespace Ploeh.Samples.Booking.WebModel.UnitTest
 {
@@ -32,6 +35,21 @@ namespace Ploeh.Samples.Booking.WebModel.UnitTest
         {
             var actual = sut.Get(year, month);
             Assert.Equal(JsonRequestBehavior.AllowGet, actual.JsonRequestBehavior);
+        }
+
+        [Theory, AutoWebData]
+        public void GetReturnsCorrectModel(
+            [Frozen]Mock<IReader<Month, IEnumerable<string>>> readerStub,
+            string[] expected,
+            DisabledDatesController sut,
+            int year,
+            int month)
+        {
+            readerStub
+                .Setup(r => r.Query(It.Is<Month>(m => m.Year == year && m.MonthNumber == month)))
+                .Returns(expected);
+            var actual = sut.Get(year, month);
+            Assert.Equal(expected, actual.Data);
         }
     }
 }
--- BookingWebModel/DisabledDatesController.cs
+++ BookingWebModel/DisabledDatesController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ploeh.Samples.Booking.DomainModel;
 
 namespace Ploeh.Samples.Booking.WebModel
 {
     public class DisabledDatesController : Controller
     {
+        private readonly IReader<Month, IEnumerable<string>> reader;
+
+        public DisabledDatesController(IReader<Month, IEnumerable<string>> reader)
+        {
+            this.reader = reader;
+        }
+
         [OutputCache(Duration = 0, VaryByParam = "none")]
         public JsonResult Get(int year, int month)
         {
-            return this.Json(new string[0], JsonRequestBehavior.AllowGet);
+            var disabledDates = reader.Query(new Month(year, month));
+            return this.Json(disabledDates.ToArray(), JsonRequestBehavior.AllowGet);
         }
     }
 }
