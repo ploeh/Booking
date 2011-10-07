ba01fcf213a6ee0944c27fbb284987cf0c8258f7
Mark Seemann <mark@seemann.ms>
2011-10-07 16:27:32 +0200

    Introduced MonthViewUpdater.

---
+++ BookingDomainModel.UnitTest/MonthViewUpdaterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+using Ploeh.Samples.Booking.DomainModel;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public class MonthViewUpdaterTests
+    {
+        [Theory, AutoDomainData]
+        public void SutIsConsumer(MonthViewUpdater sut)
+        {
+            Assert.IsAssignableFrom<IConsumer<SoldOutEvent>>(sut);
+        }
+    }
+}
---
+++ BookingDomainModel/MonthViewUpdater.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Samples.Booking.DomainModel
+{
+    public class MonthViewUpdater : IConsumer<SoldOutEvent>
+    {
+        public void Consume(SoldOutEvent item)
+        {
+        }
+    }
+}
