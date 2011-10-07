7cb027566642790524ef69dc3e7562953f2ec0cf
Mark Seemann <mark@seemann.ms>
2011-10-07 13:02:40 +0200

    Introduced Quickening for ReservationRejectedEvent.

---
+++ BookingDomainModel.UnitTest/ReservationRejectedEventQuickeningTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public class ReservationRejectedEventQuickeningTests
+    {
+        [Theory, AutoDomainData]
+        public void SutIsQuickening(ReservationRejectedEvent.Quickening sut)
+        {
+            Assert.IsAssignableFrom<IQuickening>(sut);
+        }
+    }
+}
