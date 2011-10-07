359ff4d3ce483380ea1f07686bc1bbddf2763768
Mark Seemann <mark@seemann.ms>
2011-10-07 10:17:39 +0200

    Introduced MessageConsumer.

---
+++ BookingPersistenceModel.UnitTest/MessageConsumerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Ploeh.Samples.Booking.PersistenceModel;
+using Xunit;
+
+namespace Ploeh.Samples.Booking.PersistenceModel.UnitTest
+{
+    public class MessageConsumerTests
+    {
+        [Theory, AutoPersistenceData]
+        public void SutIsObserverOfObject(MessageConsumer sut)
+        {
+            Assert.IsAssignableFrom<IObserver<object>>(sut);
+        }
+    }
+}
---
+++ BookingPersistenceModel/MessageConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Samples.Booking.PersistenceModel
+{
+    public class MessageConsumer : IObserver<object>
+    {
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(object value)
+        {
+        }
+    }
+}
