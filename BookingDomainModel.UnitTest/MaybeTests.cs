2a509fe2536bad922814f76d3b2270992fb75b34
Mark Seemann <mark@seemann.ms>
2012-02-28 15:49:13 +0100

    Added Maybe<T> monad.

---
+++ BookingDomainModel.UnitTest/MaybeTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Extensions;
+using Xunit;
+using Ploeh.Samples.Booking.DomainModel;
+using Ploeh.AutoFixture.Xunit;
+using System.Collections;
+
+namespace Ploeh.Samples.Booking.DomainModel.UnitTest
+{
+    public abstract class MaybeTests<T>
+    {
+        [Theory, AutoDomainData]
+        public void SutIsEnumerable(Maybe<T> sut)
+        {
+            Assert.IsAssignableFrom<IEnumerable<T>>(sut);
+        }
+
+        [Theory, AutoDomainData]
+        public void CreatedWithModestConstructorIsNonGenericallyEmpty(
+            [Modest]Maybe<T> sut)
+        {
+            Assert.Empty(sut);
+        }
+
+        [Theory, AutoDomainData]
+        public void CreatedWithModestConstructorIsGenericallyEmpty(
+            [Modest]Maybe<T> sut)
+        {
+            Assert.False(sut.Any());
+        }
+
+        [Theory, AutoDomainData]
+        public void CreatedWithGreedyConstructorYieldsCorrectItem(
+            [Frozen]T expected,
+            [Greedy]Maybe<T> sut)
+        {
+            Assert.Equal(expected, sut.Single());
+        }
+
+        [Theory, AutoDomainData]
+        public void CreatedWithGreedyConstructorYieldsCorrectItemOnNonGenericInterface(
+            [Frozen]T expected,
+            [Greedy]Maybe<T> sut)
+        {
+            IEnumerable actual = sut;
+            Assert.Equal(expected, actual.OfType<T>().Single());
+        }
+    }
+
+    public class MaybeTestsOfObject : MaybeTests<object> { }
+    public class MaybeTestsOfString : MaybeTests<string> { }
+    public class MaybeTestsOfInt32 : MaybeTests<int> { }
+    public class MaybeTestsOfGuid : MaybeTests<Guid> { }
+    public class MaybeTestsOfVersion : MaybeTests<Version> { }
+}
---
+++ BookingDomainModel/Maybe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Ploeh.Samples.Booking.DomainModel
+{
+    public class Maybe<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> values;
+
+        public Maybe()
+        {
+            this.values = new T[0];
+        }
+
+        public Maybe(T value)
+        {
+            this.values = new[] { value };
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
